d6a722e17a8ef8c1fdf7f9da2006d8432ffb9fc5
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-03-14 00:06:02 +0530

    pattern

--- PatternB/PatternB5.java
+++ PatternB/PatternB5.java
@@ -1,6 +1,7 @@
+package PatternB;
 import java.util.Scanner;
 
-public class PatternC {
+public class PatternB5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
--- PatternC/PatternC2.java
+++ PatternC/PatternC2.java
@@ -1,16 +1,17 @@
+package PatternC;
 import java.util.*;
 
-public class PatternD {
+public class PatternC2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=n-i; j++){
+            for(int j=1; j<=i-1; j++){
                 System.out.print(" ");
             }
 
-            for(int j=1; j<=i; j++){
+            for(int j=1; j<=n-(i-1); j++){
                 System.out.print("* ");
             }
 
@@ -20,8 +21,9 @@ public class PatternD {
 }
 
 
-//     *
-//    * *
-//   * * *
+
+// * * * * * 
 //  * * * *
-// * * * * *
+//   * * *
+//    * *
+//     *
