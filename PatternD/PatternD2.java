b985c5e2f202f7e908d3ce06de07ebb47814c613
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-03-14 07:14:05 +0530

    pattern

--- PatternD/PatternD2.java
+++ PatternD/PatternD2.java
@@ -7,13 +7,14 @@ public class PatternD2 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i=n; i>=1; i--){
-            for(int j=1; j<=n-i; j++){
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i-1; j++){
                 System.out.print(" ");
             }
 
-            for(int j=1; j<=(i*2)-1; j++){
-                if(j==1 || j==(i*2)-1 ||i==n){
+            for(int j=1; j<=(n*2)-((i*2)-1); j++){
+                if(j==1 || j==(n*2)-((i*2)-1)||i==1){
                     System.out.print("*");
                 }
                 else{
@@ -23,5 +24,29 @@ public class PatternD2 {
 
             System.out.println();
         }
+
+        // for(int i=n; i>=1; i--){
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print(" ");
+        //     }
+
+        //     for(int j=1; j<=(i*2)-1; j++){
+        //         if(j==1 || j==(i*2)-1 ||i==n){
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+
+        //     System.out.println();
+        // }
     }
 }
+
+
+// *********
+//  *     *
+//   *   *
+//    * *
+//     *
